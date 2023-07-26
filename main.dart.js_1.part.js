2bceda9bc31cb8247bf1286af0d51ca2f0683cac
kirillgladkov <crosfire2011@gmail.com>
2023-07-26 22:52:43 +0300

    edit org, some small fixes

---
+++ main.dart.js_1.part.js
@@ -0,0 +1,186 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var C={
+bgH(d){return new C.r1(d,null)},
+r1:function r1(d,e){this.c=d
+this.a=e},
+ac3:function ac3(d){var _=this
+_.d=$
+_.e=!1
+_.a=null
+_.b=d
+_.c=null},
+aNM:function aNM(d){this.a=d},
+aNN:function aNN(d,e){this.a=d
+this.b=e},
+aNH:function aNH(d){this.a=d},
+aNI:function aNI(){},
+aNJ:function aNJ(d){this.a=d},
+aNL:function aNL(d,e){this.a=d
+this.b=e},
+aNK:function aNK(d,e){this.a=d
+this.b=e}},B,A,J,D
+C=a.updateHolder(c[4],C)
+B=c[2]
+A=c[0]
+J=c[1]
+D=c[3]
+C.r1.prototype={
+a7(){return new C.ac3(B.o)}}
+C.ac3.prototype={
+aM(){this.d=this.Dk()
+this.b_()},
+Dk(){var x=0,w=A.I(y.m),v,u=2,t,s=this,r,q,p,o,n,m
+var $async$Dk=A.J(function(d,e){if(d===1){t=e
+x=u}while(true)switch(x){case 0:u=4
+p=$.aUq()
+o=s.a.c
+p=p.b
+p===$&&A.b()
+x=7
+return A.M(p.eF(0,"/offers/sale/"+o,y.b),$async$Dk)
+case 7:r=e
+o=A.b1z(r.a)
+v=o
+x=1
+break
+u=2
+x=6
+break
+case 4:u=3
+m=t
+q=A.ac(m)
+A.c0(q)
+x=6
+break
+case 3:x=2
+break
+case 6:v=null
+x=1
+break
+case 1:return A.G(v,w)
+case 2:return A.F(t,w)}})
+return A.H($async$Dk,w)},
+au3(d){var x,w,v,u=this,t=null,s="By signing this transaction you will get ",r=".\n\nThis transaction will be recorded on blockchain.",q=u.c.a0(y.k).f,p=u.c
+p.toString
+x=q.a
+w=d.c
+v=d.f
+w=A.e(w)
+A.kK(p,A.a1(A.a([B.Oc,B.aq,A.u(x===B.aV?s+w+"% of equity of @"+A.e(J.hj(v))+r:s+w+" Impact Shares of "+A.e(J.i0(v))+r,t,t,t,t,B.q4,t,t),B.lB,new A.mH(B.q8,new C.aNM(u),t),B.C,A.c2(!1,A.u("Send $"+A.e(d.d),t,t,t,t,t,t,t),B.l,t,t,t,t,t,new C.aNN(u,d),t,t)],y.u),B.aB,B.j,B.br),t,t)},
+C1(d){return this.aoU(d)},
+aoU(d){var x=0,w=A.I(y.b),v=1,u,t=[],s=this,r,q,p,o,n,m
+var $async$C1=A.J(function(e,f){if(e===1){u=f
+x=v}while(true)switch(x){case 0:s.T(new C.aNH(s))
+r=s.c.a0(y.k).f
+v=3
+p=$.aUq()
+o=d.a
+o.toString
+x=6
+return A.M(p.aoT(o,"accepted",r.a===B.aV),$async$C1)
+case 6:p=s.c
+if(p.f!=null)A.b_(p,!1).u3("/",new C.aNI(),y.q)
+t.push(5)
+x=4
+break
+case 3:v=2
+m=u
+q=A.ac(m)
+A.c0(q)
+t.push(5)
+x=4
+break
+case 2:t=[1]
+case 4:v=1
+s.T(new C.aNJ(s))
+x=t.pop()
+break
+case 5:return A.G(null,w)
+case 1:return A.F(u,w)}})
+return A.H($async$C1,w)},
+G(d){var x=this.d
+x===$&&A.b()
+return new A.cu(A.d5(new C.aNL(this,d),x,y.m),"Offer to Buy",null)}}
+var z=a.updateTypes([])
+C.aNM.prototype={
+$0(){var x=this.a.c
+x.toString
+A.b_(x,!1).dI()},
+$S:0}
+C.aNN.prototype={
+$0(){var x=this.a,w=x.c
+w.toString
+A.b_(w,!1).dI()
+x.C1(this.b)},
+$S:0}
+C.aNH.prototype={
+$0(){this.a.e=!0},
+$S:0}
+C.aNI.prototype={
+$1(d){return!1},
+$S:12}
+C.aNJ.prototype={
+$0(){this.a.e=!1},
+$S:0}
+C.aNL.prototype={
+$2(d,e){var x,w,v,u,t,s,r,q,p,o,n,m,l=null,k=e.b
+if(k==null)return B.eu
+x=this.b
+w=k
+v=w.e
+v.toString
+u=J.aK(v,"avatar")
+if(u==null){v=w.e
+v.toString
+u=J.aK(v,"logo")}v=w.e
+v.toString
+t=J.aK(v,"nickname")
+if(t==null){v=w.e
+v.toString
+t=J.aK(v,"username")}v=A.aV(20)
+s=A.aV(15)
+s=A.aw(l,A.d4(B.A,u!=null?new A.dv($.e6().ge4()+A.e(u),l):l,B.l,B.ax),B.a1,l,l,new A.aW(B.B,l,l,s,l,l,B.z),l,40,l,l,l,l,40)
+w=w.e
+w.toString
+r=y.u
+v=A.aw(l,A.a1(A.a([A.ak(A.a([s,B.bQ,A.a1(A.a([A.a0D(l,l,B.cC,l,l,!0,l,A.d6(A.a([D.aBg,A.d6(l,B.NO," "+A.e(J.aK(w,"name")))],y.F),B.P,l),B.X,l,l,1,B.bk),B.Nc,A.u("@"+A.e(t),l,l,l,l,A.z(x).p3.at.eJ(B.B,B.T),l,l)],r),B.N,B.j,B.f)],r),B.i,B.j,B.f)],r),B.i,B.j,B.f),B.l,l,l,new A.aW(B.aQ,l,l,v,l,l,B.z),l,l,l,B.ex,l,l,l)
+w=k
+q=x.a0(y.k).f
+s=w.c
+s.toString
+p=B.e.ai(s*1e9/w.f.gpP()*100,1)
+s=A.aV(20)
+o=A.aV(20)
+n=$.c6().ge4()
+m=w.f.gfL()
+m.toString
+x=A.a([A.ak(A.a([A.aw(l,A.d4(B.A,new A.dv(n+m,l),B.l,B.ax),B.a1,l,l,new A.aW(B.B,l,l,o,l,l,B.z),l,60,l,l,l,l,60),B.cA,A.bb(A.a1(A.a([A.u(A.e(J.i0(w.f)),1,B.af,l,l,B.P,l,l),A.u("@"+A.e(J.hj(w.f)),l,l,l,l,A.z(x).p3.at.eJ(B.B,B.T),l,l)],r),B.N,B.j,B.f),1,l)],r),B.i,B.j,B.f),B.aq,B.c5,B.O],r)
+if(q.a===B.ah)x.push(A.a1(A.a([A.ak(A.a([B.lM,A.u(A.e(w.c),l,l,l,l,B.P,l,l)],r),B.i,B.L,B.f),B.aq],r),B.i,B.j,B.f))
+x.push(A.ak(A.a([A.u(q.a===B.ah?"Equity to Date":"Equity",l,l,l,l,l,l,l),A.u(p+"%",l,l,l,l,B.q5,l,l)],r),B.i,B.L,B.f))
+x.push(B.O)
+x.push(B.c5)
+x.push(B.O)
+x.push(A.ak(A.a([B.q7,A.u("$"+A.e(w.d),l,l,l,l,B.P,l,l)],r),B.i,B.L,B.f))
+x=A.a([B.O,v,B.bt,A.aw(l,A.a1(x,B.i,B.j,B.f),B.l,l,l,new A.aW(B.aQ,l,l,s,l,l,B.z),l,l,l,B.ex,l,l,l),B.O,B.pE],r)
+x=A.a([A.qM(0,A.a1(A.a([A.bb(new A.eA(x,B.G,l),1,l)],r),B.i,B.j,B.f),l)],r)
+w=this.a
+v=w.e
+w=v?l:new C.aNK(w,e)
+x.push(A.h9(0,A.c7(A.bp(A.c2(!1,v?B.c2:A.u("Buy for $"+A.e(k.d),l,l,l,l,l,l,l),B.l,l,l,l,l,l,w,l,l),l,290),l,l),l,l,0,0,l,l))
+return A.er(B.be,x,B.E,B.aZ,l)},
+$S:764}
+C.aNK.prototype={
+$0(){var x=this.b.b
+if(x==null)x=y.h.a(x)
+return this.a.au3(x)},
+$S:0};(function inheritance(){var x=a.inherit,w=a.inheritMany
+x(C.r1,A.a_)
+x(C.ac3,A.a2)
+w(A.Eo,[C.aNM,C.aNN,C.aNH,C.aNJ,C.aNK])
+x(C.aNI,A.lZ)
+x(C.aNL,A.xF)})()
+A.aXi(b.typeUniverse,JSON.parse('{"r1":{"a_":[],"h":[]},"ac3":{"a2":["r1"]}}'))
+var y={k:A.a3("xL"),F:A.a3("r<ia>"),u:A.a3("r<h>"),h:A.a3("r0"),b:A.a3("@"),q:A.a3("O?"),m:A.a3("r0?")};(function constants(){D.Ri="/saleoffer"
+D.aCM=new A.t(!0,B.aK,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null)
+D.aBg=new A.kq("from",null,B.bS,D.aCM)})()}
+$__dart_deferred_initializers__["g079TPl8woPi6A162xEmlg/5Hz4="] = $__dart_deferred_initializers__.current
---
+++ main.dart.js_2.part.js
@@ -0,0 +1,69 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B={
+bgC(d){return new B.r_(d,null)},
+r_:function r_(d,e){this.c=d
+this.a=e},
+abV:function abV(d){this.a=null
+this.b=d
+this.c=null},
+aNa:function aNa(){},
+aNb:function aNb(d){this.a=d}},C,A,D,J
+B=a.updateHolder(c[5],B)
+C=c[2]
+A=c[0]
+D=c[6]
+J=c[1]
+B.r_.prototype={
+a7(){return new B.abV(C.o)}}
+B.abV.prototype={
+DK(d){var x=null,w=this.c.a0(y.f)
+w.toString
+w.f.ig(A.iS(x,x,x,C.cZ,x,C.E,C.q,A.u(d,x,x,x,x,x,x,x),C.bU,D.X4,x,x,x,x,x,x,!0,x))},
+wl(){var x=0,w=A.I(y.b),v=1,u,t=this,s,r,q,p,o,n
+var $async$wl=A.J(function(d,e){if(d===1){u=e
+x=v}while(true)switch(x){case 0:v=3
+x=6
+return A.M($.e6().l1(t.a.c),$async$wl)
+case 6:s=e
+x=7
+return A.M($.kM().or(0,"jwt_token",s.b),$async$wl)
+case 7:p=t.c
+if(p.f!=null)A.b_(p,!1).u3("/",new B.aNa(),y.a)
+v=1
+x=5
+break
+case 3:v=2
+n=u
+p=A.ac(n)
+if(p instanceof A.f5){r=p
+p=r.b
+p=p==null?null:J.aK(p.a,"message")
+t.DK(p==null?r.f:p)
+throw n}else{q=p
+t.DK(J.cv(q))
+throw n}x=5
+break
+case 2:x=1
+break
+case 5:return A.G(null,w)
+case 1:return A.F(u,w)}})
+return A.H($async$wl,w)},
+G(d){return new A.cu(A.d5(new B.aNb(d),this.wl(),y.b),"",null)}}
+var z=a.updateTypes([])
+B.aNa.prototype={
+$1(d){return!1},
+$S:12}
+B.aNb.prototype={
+$2(d,e){var x=null
+if(e.c!=null)return A.c7(A.u("Ups...\nSomething went wrong...",x,x,x,x,A.z(this.a).p3.d.aru(C.B,"Gilroy",D.Yc),C.a3,x),x,x)
+if(e.b==null)return C.eu
+return C.eu},
+$S:765};(function inheritance(){var x=a.inherit
+x(B.r_,A.a_)
+x(B.abV,A.a2)
+x(B.aNa,A.lZ)
+x(B.aNb,A.xF)})()
+A.aXi(b.typeUniverse,JSON.parse('{"r_":{"a_":[],"h":[]},"abV":{"a2":["r_"]}}'))
+var y={f:A.a3("CJ"),b:A.a3("@"),a:A.a3("O?")};(function constants(){D.X4=new A.bf(31536e9)
+D.Yc=new A.FA(1,"italic")})()}
+$__dart_deferred_initializers__["kYMu0BWd4KhyvTA0s2p9M6zdOr4="] = $__dart_deferred_initializers__.current
